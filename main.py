f7f6f73180a0f25b6649ef73ad68aca4530a0e5f
ji3g4bp6 <holydark0914@gmail.com>
2025-08-13 14:38:25 +0800

    fixed TypeERROR

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
 logging.basicConfig(
-    level=logging.info,
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
         logging.FileHandler(os.path.join(log_dir, "app.log")),
