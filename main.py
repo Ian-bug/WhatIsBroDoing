4bb4b7891d8a5dbe6e8378954015219e9ffce819
ji3g4bp6 <holydark0914@gmail.com>
2025-08-13 14:51:17 +0800

    yeah

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.ERROR,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
         logging.FileHandler(os.path.join(log_dir, "app.log")),
